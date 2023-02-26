3b7b2463bb591ae60e01edc71a4a20cc8792392a
Yufan-99 <123699910+Yufan-99@users.noreply.github.com>
2023-02-26 16:58:27 +0100

    Update download_data.R
    I added the codes of downloading the data of detailed listing and calendar in Paris,  Amsterdam and NY. For each url, I assign the name as Paris_listing, Paris_calendar, Amsterdam_listing, Amsterdam_calendar, NY_listing and NY_calendar

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,8 +1,41 @@
-# Download dataset 1
-# dir.create('./data/dataset1')  # Uncomment if need to create directory with R
-download.file('https://rgreminger.github.io/files/dataset1.csv','./data/dataset1/dataset1.csv')
+## Create data directory
+dir.create("./data/raw_data")
 
-# Download dataset 2
-# dir.create('./data/dataset2')  # Uncomment if need to create directory with R
-download.file('https://rgreminger.github.io/files/dataset2.csv','./data/dataset2/dataset2.csv')
+## Load libraries
+library(tidyverse)
+library(dplyr)
+
+## Load the URLs
+options(timeout = max(1000, getOption("timeout")))
+urls <- c(
+  "http://data.insideairbnb.com/france/ile-de-france/paris/2022-12-10/data/listings.csv.gz",
+  "http://data.insideairbnb.com/france/ile-de-france/paris/2022-12-10/data/calendar.csv.gz",
+  "http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-12-05/data/listings.csv.gz",
+  "http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-12-05/data/calendar.csv.gz",
+  "http://data.insideairbnb.com/united-states/ny/new-york-city/2022-12-04/data/listings.csv.gz",
+  "http://data.insideairbnb.com/united-states/ny/new-york-city/2022-12-04/data/calendar.csv.gz"
+)
+
+## Names for each URL
+names <- c("Paris_listing", "Paris_calendar", "Amsterdam_listing", "Amsterdam_calendar", "NY_listing", "NY_calendar")
+
+## Loop through URLs
+for (i in 1:length(urls)) {
+  download.file(urls[i], paste0(names[i], ".csv.gz"), mode = "wb")
+}
+
+## View Data 
+Paris_listing <- read.csv(gzfile("Paris_listing.csv.gz"), header = TRUE, sep = ",")
+View(Paris_listing)
+Paris_calendar <- read.csv(gzfile("Paris_calendar.csv.gz"), header = TRUE, sep = ",")
+View(Paris_calendar)
+Amsterdam_listing <- read.csv(gzfile("Amsterdam_listing.csv.gz"), header = TRUE, sep = ",")
+View(Amsterdam_listing)
+Amsterdam_calendar <- read.csv(gzfile("Amsterdam_calendar.csv.gz"), header = TRUE, sep = ",")
+View(Amsterdam_calendar)
+NY_listing <- read.csv(gzfile("NY_listing.csv.gz"), header = TRUE, sep = ",")
+View(NY_listing)
+NY_calendar <- read.csv(gzfile("NY_calendar.csv.gz"), header = TRUE, sep = ",")
+View(NY_calendar)
+
 

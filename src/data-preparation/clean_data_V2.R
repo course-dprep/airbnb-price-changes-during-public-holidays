6795b9e9f41f45faa3f167fd3699fada1393b027
Yufan-99 <123699910+Yufan-99@users.noreply.github.com>
2023-03-06 18:42:39 +0100

    Update clean_data_V2.R
    In the previous version, the calendar dataset and listing dataset in each cities are cleaned firstly, then they are merged together, so the codes are repetitive and tedious. Now I updated the codes by merging the dataset from each city firstly, then clean the merged dataset at once.

--- src/data-preparation/clean_data_V2.R
+++ src/data-preparation/clean_data_V2.R
@@ -3,73 +3,52 @@ library(tidyverse)
 library(dplyr)
 library(readr)
 
-# Modify the calendar file for three cities
-##Data subset with the variables of listing_id,date and price from NY_calendar, Amsterdam_calendar and Paris_calendar
-NY_calendar_new <- NY_calendar %>% 
+## Add the new column named city in the NY_calendar, Paris_calendar, Amsterdam_calendar
+NY_calendar <- NY_calendar %>% mutate(city="NY")
+Paris_calendar <- Paris_calendar %>% mutate(city="Paris")
+Amsterdam_calendar <- Amsterdam_calendar %>% mutate(city="Amsterdam")
+
+# Merge three calendars file into one integrated calendar
+calendar <- bind_rows (NY_calendar) %>% bind_rows(Paris_calendar) %>% bind_rows(Amsterdam_calendar)
+
+#calendar subset with the variables of listing_id,date and price.
+calendar <- calendar %>% 
   mutate(date=as.Date(date)) %>% 
-  group_by(listing_id) %>%
-  select(listing_id,date,price)
-NY_calendar_new <- NY_calendar_new[order(NY_calendar_new$listing_id),]
+  group_by(listing_id,date) %>%
+  select(listing_id,date,price,city)
+calendar <- calendar[order(calendar$listing_id),]
 
-Paris_calendar_new <- Paris_calendar %>%
-  mutate(date=as.Date(date)) %>% 
-  group_by(listing_id) %>%
-  select(listing_id,date,price)
-Paris_calendar_new<- Paris_calendar_new[order(Paris_calendar_new$listing_id),]
-
-Amsterdam_calendar_new <- Amsterdam_calendar %>%
-  mutate(date=as.Date(date)) %>% 
-  group_by(listing_id) %>%
-  select(listing_id,date,price)
-Amsterdam_calendar_new <- Amsterdam_calendar_new[order(Amsterdam_calendar_new$listing_id),]
-
-## Add the new column named city in the NY_calendar_new, Paris_calendar_new, Amsterdam_calendar_new
-NY_calendar_new <- NY_calendar_new %>% mutate(city="NY")
-Paris_calendar_new <- Paris_calendar_new %>% mutate(city="Paris")
-Amsterdam_calendar_new <- Amsterdam_calendar_new %>% mutate(city="Amsterdam")
-
-# Merge three updated calendar file to one integrate dataframe for calendar
-Calendar <- bind_rows (NY_calendar_new) %>% bind_rows(Paris_calendar_new) %>% bind_rows(Amsterdam_calendar_new)
-
-## Shows classes variables (Price was both character)
-class(Calendar$price)
-
-## We change the price from character to numeric
-Calendar$price <- as.numeric(gsub("\\$", "", Calendar$price))
+## Change the price from character to numeric
+calendar$price <- as.numeric(gsub("\\$", "", calendar$price))
 
 ## Shows the number of NA's 
-sum(is.na(Calendar))
+sum(is.na(calendar))
 
 ## cleares the NA's in Calendar
-Calendar <- na.omit(Calendar)
-View(Calendar)
-
-# Select the columns of id, host_is_superhost, room_type and accommodates from NY_listing,Paris_listing and Amsterdam_listing.
-NY_listing_new <- NY_listing %>% select(id, host_is_superhost, room_type,accommodates)
-Paris_listing_new <- Paris_listing %>% select(id, host_is_superhost, room_type,accommodates)
-Amsterdam_listing_new <- Amsterdam_listing %>% select(id, host_is_superhost, room_type,accommodates)
+calendar <- na.omit(calendar)
+View(calendar)
 
 # Merge three listings file into one file named all_listing
-all_listing <- bind_rows(NY_listing_new) %>% bind_rows(Paris_listing_new) %>% bind_rows(Amsterdam_listing_new)
+all_listing <- bind_rows(NY_listing) %>% bind_rows(Paris_listing) %>% bind_rows(Amsterdam_listing)
 
-# Combine the calendar dataset and the listing dataset into one moderate dataset, with the columns of listing_id, date, price,city, host_is_superhost,room_type and accomodates.
-moderate_dataset <- Calendar %>%
+# Select the columns of id, host_is_superhost, room_type and accommodates from all_listing
+all_listing <- all_listing %>% select(id, host_is_superhost, room_type,accommodates)
+
+# Combine the calendar and the all_listing  into one moderate dataset, with the columns of listing_id, date, price,city, host_is_superhost,room_type and accommodates.
+moderate_dataset <- calendar %>%
   inner_join(all_listing %>% select(id, host_is_superhost, room_type, accommodates), 
-            by = c("listing_id" = "id"))
-View(moderate_dataset)
+             by = c("listing_id" = "id"))
+View(moderate_dataset) 
 
 ## moderate dataset separated by cities
 NY_dataset<- moderate_dataset %>% 
-  filter(city=="NY") %>% 
-  select(listing_id, date, price, city, host_is_superhost, room_type, accommodates)
+  filter(city=="NY")
 View(NY_dataset)
 
 Paris_dataset<- moderate_dataset %>% 
-  filter(city=="Paris") %>% 
-  select(listing_id, date, price, city, host_is_superhost, room_type, accommodates)
+  filter(city=="Paris") 
 View(Paris_dataset)
 
 Amsterdam_dataset<- moderate_dataset %>% 
-  filter(city=="Amsterdam") %>% 
-  select(listing_id, date, price, city, host_is_superhost, room_type, accommodates)
+  filter(city=="Amsterdam") 
 View(Amsterdam_dataset)

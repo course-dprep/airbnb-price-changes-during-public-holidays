5870299662078d1a44af8e24ed81344d861fc83d
BastiaanVanHouten <123388277+BastiaanVanHouten@users.noreply.github.com>
2023-03-06 14:12:52 +0100

    Update Anova_date_and_price.R
    With interaction terms

--- src/data-preparation/Anova_date_and_price.R
+++ src/data-preparation/Anova_date_and_price.R
@@ -1,38 +1,62 @@
 ## Creates DF of the cities only first 1000000 observations
-df_subset_NY<- head(NY_calendar, 1000000)
-df_subset_AM<- head(Amsterdam_calendar, 1000000)
-df_subset_PR<- head(Paris_calendar, 1000000)
+NY_calendar_new<- head(NY_calendar, 1000000)
+Amsterdam_calendar_new<- head(Amsterdam_calendar, 1000000)
+Paris_calendar_new<- head(Paris_calendar, 1000000)
+
+## Add the new column named city in the NY_calendar_new, Paris_calendar_new, Amsterdam_calendar_new
+NY_calendar_new <- NY_calendar_new %>% mutate(city="NY")
+Paris_calendar_new <- Paris_calendar_new %>% mutate(city="Paris")
+Amsterdam_calendar_new <- Amsterdam_calendar_new %>% mutate(city="Amsterdam")
+
 
 ## Creates a dataframe with the different dataframes
-subsets<- df_subset_PR%>%
-  bind_rows(df_subset_AM)%>%
-  bind_rows(df_subset_NY)
+calendar<- NY_calendar_new%>%
+  bind_rows(Paris_calendar_new )%>%
+  bind_rows(Amsterdam_calendar_new)
 
-## Shows classes variables (Price and Date were both character)
-class(subsets$date)
-class(subsets$price)
+## Filter calendar 
+calendar<- calendar%>%
+  select(listing_id,date,price, city)
+
+## Shows classes variables
+class(calendar$date)
+class(calendar$price)
 
 ## We change the price from character to numeric
-subsets$price <- as.numeric(gsub("\\$", "", subsets$price))
+calendar$price <- as.numeric(gsub("\\$", "", calendar$price))
 
 ## Shows the number of NA's 
-sum(is.na(subsets))
+sum(is.na(calendar))
 
 ## cleares the NA's 
-clean_data <- na.omit(subsets)
+calendar <- na.omit(calendar)
 
 ## Changes the date into a numeric date variable
-clean_data$date <- as.Date(clean_data$date)
+calendar$date <- as.Date(Calendar$date)
 
 ## Add groups Group 1 New years, Group 2 Easter, Group 3 other
-clean_data<- clean_data %>%
+calendar<- calendar %>%
   mutate(group = case_when(
-    between(date, as.Date("2022-12-28"), as.Date("2023-01-05")) ~ "1",
-    between(date, as.Date("2023-04-09"), as.Date("2023-04-18")) ~ "2",
-    TRUE ~ "3"
+    between(date, as.Date("2022-12-23"), as.Date("2022-12-27")) ~ "1",
+    between(date, as.Date("2022-12-29"), as.Date("2023-01-03")) ~ "2",
+    between(date, as.Date("2023-04-12"), as.Date("2023-04-16")) ~ "3",
+    TRUE ~ "4"
   )) 
-  
 
-View(clean_data)
-model <- lm(price ~ group + minimum_nights + available , data = clean_data)
+
+# Merge three listings file into one file named all_listing
+all_listing <- bind_rows(NY_listing) %>% bind_rows(Paris_listing) %>% bind_rows(Amsterdam_listing)
+
+# Select the columns of id, host_is_superhost, room_type and accommodates from NY_listing,Paris_listing and Amsterdam_listing.
+all_listing_new <- all_listing %>% select(id, host_is_superhost, room_type,accommodates)
+
+
+# Combine the calendar dataset and the listing dataset into one moderate dataset, with the columns of listing_id, date, price,city, host_is_superhost,room_type and accomodates.
+moderate_dataset <- calendar %>%
+  inner_join(all_listing %>% select(id, host_is_superhost, room_type, accommodates), 
+             by = c("listing_id" = "id"))
+View(moderate_dataset)  
+
+## Run the anova
+model <- lm(price ~ group + group*host_is_superhost + group*room_type + group*accommodates + group*city, moderate_dataset)
 anova(model)

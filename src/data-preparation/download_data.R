5ad81bb1bb8469ccab591ded3f96c9153c65e772
Yufan-99 <y.wang_34@tilburguniversity.edu>
2023-03-16 18:18:37 +0100

    add a directory line to move all the raw data to data folder

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -16,9 +16,8 @@ urls <- c(
 ## Names for each URL
 names <- c("Paris_listing", "Paris_calendar", "Amsterdam_listing", "Amsterdam_calendar", "NY_listing", "NY_calendar")
 
-## Loop through URLs
 for (i in 1:length(urls)) {
-  download.file(urls[i], paste0(names[i], ".csv.gz"), mode = "wb")
-}
-
+  download.file(urls[i], paste0("../../data/", names[i], ".csv.gz"), mode = "wb")
+  }
+  
 

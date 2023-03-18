458feec6a5bafe4f89cf98ca28e30a7e456a5e55
mynitran <m.n.tran@tilburguniversity.edu>
2023-03-18 23:02:42 +0100

    changed accommodates_dummy to accomodates in analyze.R

--- src/analysis/analyze.R
+++ src/analysis/analyze.R
@@ -10,14 +10,14 @@ Amsterdam_dataset<- read_csv("../../gen/data-preparation/output/Amsterdam_datase
 NY_dataset<-read_csv("../../gen/data-preparation/output/NY_dataset.csv")
 
 # 
-Paris_model <- lm(price ~ group + host_is_superhost + accommodates_dummy + room_type + group*host_is_superhost + group*accommodates_dummy + group*room_type, data =Paris_dataset)
+Paris_model <- lm(price ~ group + host_is_superhost + accommodates + room_type + group*host_is_superhost + group*accommodates + group*room_type, data =Paris_dataset)
 Paris_result<- anova(Paris_model)
 Paris_result
 
-Amsterdam_model <- lm(price ~ group + host_is_superhost + accommodates_dummy + room_type + group*host_is_superhost + group*accommodates_dummy + group*room_type, data =Amsterdam_dataset)
+Amsterdam_model <- lm(price ~ group + host_is_superhost + accommodates + room_type + group*host_is_superhost + group*accommodates + group*room_type, data =Amsterdam_dataset)
 Amsterdam_result<- anova(Amsterdam_model)
 Amsterdam_result
 
-NY_model<- lm(price ~ group + host_is_superhost + accommodates_dummy + room_type + group*host_is_superhost + group*accommodates_dummy + group*room_type, data =NY_dataset)
+NY_model<- lm(price ~ group + host_is_superhost + accommodates + room_type + group*host_is_superhost + group*accommodates + group*room_type, data =NY_dataset)
 NY_result<-anova(NY_model)
 NY_result

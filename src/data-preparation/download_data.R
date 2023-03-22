86e7ac13ef9cf9c737b1d4635f357f3812d5323e
BastiaanVanHouten <b.a.vanhouten@tilburguniversity.edu>
2023-03-22 13:05:35 +0100

    I changed the src. code aso it creates itss own directorirs and I adjusted the makefille accordingly

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(dplyr)
 
-dir.create("data")
+dir.create("../../data")
 
 ## Load the URLs
 options(timeout = max(1000, getOption("timeout")))
@@ -15,12 +15,11 @@ urls <- c(
   "http://data.insideairbnb.com/united-states/ny/new-york-city/2022-12-04/data/calendar.csv.gz"
 )
 
-
 ## Names for each URL
 names <- c("Paris_listing", "Paris_calendar", "Amsterdam_listing", "Amsterdam_calendar", "NY_listing", "NY_calendar")
 
 for (i in 1:length(urls)) {
-  download.file(urls[i], paste0("./data/", names[i], ".csv.gz"), mode = "wb")
-  }
+  download.file(urls[i], paste0("../../data/", names[i], ".csv.gz"), mode = "wb")
+}
   
 

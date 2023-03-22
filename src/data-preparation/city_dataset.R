dfb8c43bbd61be37084ff26a340135388b654c49
BastiaanVanHouten <b.a.vanhouten@tilburguniversity.edu>
2023-03-22 11:58:14 +0100

    Changed all the src. so it actually also creates the directory where we store the files in, adjusted calander src so it creates the dataset group_calander, it puts it in gen/ temp files

--- src/data-preparation/city_dataset.R
+++ src/data-preparation/city_dataset.R
@@ -1,10 +1,12 @@
 library(dplyr)
 library(readr)
 
+dir.create("./gen/data-preparation/output/", recursive = TRUE)
+
 # Set up file paths and city names
-file_paths <- c("../../gen/data-preparation/temp/Paris_calendar.csv",
-                "../../gen/data-preparation/temp/Amsterdam_calendar.csv",
-                "../../gen/data-preparation/temp/NY_calendar.csv")
+file_paths <- c("./gen/data-preparation/temp/Paris_group_calendar.csv",
+                "./gen/data-preparation/temp/Amsterdam_group_calendar.csv",
+                "./gen/data-preparation/temp/NY_group_calendar.csv")
 
 city_names <- c("Paris", "Amsterdam", "NY")
 
@@ -19,7 +21,7 @@ for (i in seq_along(file_paths)) {
 listing_data <- list()
 
 for (city in city_names) {
-  listing_data[[city]] <- read_csv(gzfile(paste0("../../data/", city, "_listing.csv.gz")), col_names = TRUE)
+  listing_data[[city]] <- read_csv(gzfile(paste0("./data/", city, "_listing.csv.gz")), col_names = TRUE)
 }
 
 # Combine the calendar and listing data for each city and write the resulting dataset to a csv file
@@ -29,6 +31,6 @@ for (i in seq_along(city_names)) {
     inner_join(listing_data[[city]] %>% select(id, host_is_superhost, room_type, accommodates), 
                by = c("listing_id" = "id")) %>%
     mutate(accommodates = ifelse(accommodates > 10, 1, 0))
-  write_csv(dataset, paste0('../../gen/data-preparation/output/', city, "_dataset.csv"))
+  write_csv(dataset, paste0('./gen/data-preparation/output/', city, "_dataset.csv"))
 }
  

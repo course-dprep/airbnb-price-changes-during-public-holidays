5236337f8a0c3c9b8670b0d3d9844fc82def5164
BastiaanVanHouten <b.a.vanhouten@tilburguniversity.edu>
2023-03-22 11:32:57 +0100

    changed the make file so it takes the right directories and uses variable names

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -2,6 +2,8 @@
 library(tidyverse)
 library(dplyr)
 
+dir.create("data")
+
 ## Load the URLs
 options(timeout = max(1000, getOption("timeout")))
 urls <- c(
@@ -13,11 +15,12 @@ urls <- c(
   "http://data.insideairbnb.com/united-states/ny/new-york-city/2022-12-04/data/calendar.csv.gz"
 )
 
+
 ## Names for each URL
 names <- c("Paris_listing", "Paris_calendar", "Amsterdam_listing", "Amsterdam_calendar", "NY_listing", "NY_calendar")
 
 for (i in 1:length(urls)) {
-  download.file(urls[i], paste0("../../data/", names[i], ".csv.gz"), mode = "wb")
+  download.file(urls[i], paste0("./data/", names[i], ".csv.gz"), mode = "wb")
   }
   
 
